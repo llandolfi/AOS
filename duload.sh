d9f5c9d75c6351b141b7fdb5494dae66b682984f
llandolfi <lor.landolfi@gmail.com>
2018-07-26 18:10:23 +0200

    adding du load

---
+++ duload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
+while true; do taskset -c 3 /bin/du / ; done  &
